272125b9e867febee28bde485a20d24060a90719
Pascal Sommer <p@pascalsommer.ch>
2022-12-29 17:18:49 +0100

    Add link to favourites from social links bar

--- laravel/resources/views/layouts/social-links.blade.php
+++ laravel/resources/views/layouts/social-links.blade.php
@@ -1,9 +1,7 @@
 <p>
     <a title="Soundcloud" href="https://soundcloud.com/pascal-sommer" class="external-platform-link">@include('icons.soundcloud')</a>
-    <a title="Twitter" href="https://twitter.com/sommerpascal" class="external-platform-link">@include('icons.twitter')</a>
     <a title="Github" href="https://github.com/pascal-so" class="external-platform-link">@include('icons.github')</a>
-    <a title="Medium" href="https://medium.com/@pascal.sommer.ch" class="external-platform-link">@include('icons.medium')</a>
-    <a title="Google Play" href="https://play.google.com/store/apps/developer?id=Pascal+Sommer" class="external-platform-link">@include('icons.googleplay')</a>
+    <a class="tag" style="margin-bottom: 0" href="{{ route('filtered', ['tags' => 'Favourites']) }}">Favourites</a>
     <a title="Youtube" href="https://youtube.com/pascalsommermovies" class="external-platform-link">@include('icons.youtube')</a>
     <a title="RSS feed - {{ config('feed.feeds.posts.title') }}" type="application/atom+xml" href="{{ asset('feed') }}" class="external-platform-link">@include('icons.rss')</a>
 </p>
