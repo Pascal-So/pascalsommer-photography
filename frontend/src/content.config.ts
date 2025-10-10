fe5f72d3326952507ccedaed4ca7a27ad8f693b8
Pascal Sommer <p@pascalsommer.ch>
2025-10-10 14:10:47 +0200

    basic post loading

--- frontend/src/content.config.ts
+++ frontend/src/content.config.ts
@@ -1,22 +1,17 @@
 import { file } from "astro/loaders";
-import { defineCollection, reference, z } from "astro:content";
+import { defineCollection, z } from "astro:content";
 
 const posts = defineCollection({
   loader: file("src/data/example-posts/data.json", { parser: (text) => JSON.parse(text).posts }),
-  schema: z.object({
+  schema: ({image}) => z.object({
     name: z.string(),
     date: z.string().transform((str) => new Date(str)),
-    photos: z.array(reference("photos")),
+    photos: z.array(z.object({
+      id: z.number(),
+      description: z.string(),
+      img: image(),
+    })),
   }),
 });
 
-const photos = defineCollection({
-  loader: file("src/data/example-posts/data.json", { parser: (text) => JSON.parse(text).photos }),
-  schema: z.object({
-    post: reference("posts"),
-    description: z.string(),
-    img: z.string(),
-  }),
-});
-
-export const collections = { posts, photos }
+export const collections = { posts }
