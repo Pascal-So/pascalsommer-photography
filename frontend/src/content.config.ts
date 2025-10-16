59ec69e8df2cce78fb5b0443206de420f5d54378
Pascal Sommer <p@pascalsommer.ch>
2025-10-16 18:04:31 +0200

    feat: tag display & filter

--- frontend/src/content.config.ts
+++ frontend/src/content.config.ts
@@ -1,9 +1,9 @@
 import { file } from "astro/loaders";
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 import mysql, { type ConnectionOptions } from "mysql2/promise";
 import fs from "node:fs/promises";
 
-const importFromDB = false;
+const importFromDB = true;
 if (importFromDB) {
   const connOptions: ConnectionOptions = {
     user: import.meta.env.DB_USER,
@@ -17,7 +17,17 @@ if (importFromDB) {
   const photos = (
     await conn.query("select * from photos order by weight")
   )[0] as any;
-  const posts = (await conn.query("select * from posts"))[0] as any;
+  const posts = (await conn.query("select * from posts"))[0] as any[];
+  const tags = (await conn.query("select * from tags"))[0] as any[];
+  const photosTags = (await conn.query("select * from photo_tag"))[0] as any[];
+
+  const tagsMap = Object.fromEntries(
+    tags.map((tag: any) => [
+      tag.id.toString(),
+      { id: tag.name, order: tag.id },
+    ]),
+  );
+
   const postsMap = Object.fromEntries(
     posts.map((post: any) => [
       post.id.toString(),
@@ -30,13 +40,15 @@ if (importFromDB) {
         id: photo.id,
         description: photo.description,
         img: `/src/data/real-data/img/${photo.path}`,
+        tags: photosTags.filter((pt) => pt.photo_id === photo.id).map((pt) => tagsMap[pt.tag_id].id),
       });
     }
   }
   const postsCollection = Object.values(postsMap);
+  const tagsCollection = Object.values(tagsMap);
   await fs.writeFile(
     "src/data/real-data/data.json",
-    JSON.stringify({ posts: postsCollection }),
+    JSON.stringify({ posts: postsCollection, tags: tagsCollection }),
   );
 }
 
@@ -53,9 +65,21 @@ const posts = defineCollection({
           id: z.number(),
           description: z.string(),
           img: image(),
+          tags: z.array(reference("tags")),
         }),
       ),
     }),
 });
 
-export const collections = { posts };
+const tags = defineCollection({
+  loader: file("src/data/real-data/data.json", {
+    parser: (text) => JSON.parse(text).tags,
+  }),
+  schema: () =>
+    z.object({
+      id: z.string(),
+      order: z.number(),
+    }),
+});
+
+export const collections = { posts, tags };
