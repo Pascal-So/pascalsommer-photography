91b0345bbbd1b07b6ad52ac1f045aa3f4ccc5dfe
Pascal Sommer <p@pascalsommer.ch>
2025-10-25 01:15:46 +0200

    fix: nginx proxy issue with laravel

--- backend/laravel/app/Http/Middleware/TrustProxies.php
+++ backend/laravel/app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = ['172.18.0.0/12']; // allow nginx proxy in docker compose
 
     /**
      * The headers that should be used to detect proxies.
