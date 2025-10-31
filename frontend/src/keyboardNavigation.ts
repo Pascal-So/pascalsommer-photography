b60a8f34988e4eb938384f02291b169b9ebe9117
Pascal Sommer <p@pascalsommer.ch>
2025-10-31 15:59:37 +0100

    fix: arrow key navigation without esnext, fix some type errors

--- frontend/src/keyboardNavigation.ts
+++ frontend/src/keyboardNavigation.ts
@@ -1,8 +1,8 @@
 function activateKeyboardNavigation() {
   const elements = document.querySelectorAll("[data-shortcut-key]");
-  const htmlElements = elements
-    .values()
-    .filter((el): el is HTMLElement => el instanceof HTMLElement);
+  const htmlElements = [...elements.values()].filter(
+    (el): el is HTMLElement => el instanceof HTMLElement,
+  );
   const keyMap = new Map(
     htmlElements.map((el) => [el.getAttribute("data-shortcut-key"), el]),
   );
--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,5 +1,5 @@
 export function formatDate(date: Date): string {
-  return date.toISOString().split("T")[0];
+  return date.toISOString().split("T")[0]!;
 }
 
 export type DescriptionElement =
@@ -23,7 +23,9 @@ export function parseDescription(desc: string): DescriptionElement[] {
 
     // check the type of the matched segment
     if (match[2] === "photo" || match[2] === "post") {
-      segments.push({ linktype: match[2], id: parseInt(match[3]) });
+      const linktype = match[2];
+      const id = parseInt(match[3]!);
+      segments.push({ linktype, id });
     } else if (match[4] === "coords") {
       segments.push({ linktype: match[4], latlng: match[5] });
     }
